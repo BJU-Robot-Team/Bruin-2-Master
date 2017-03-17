a76b57bb9ace754badef556422942e6a066329e3
Bruin-2 Development Laptop <BjuBruinsRobotTeam@gmail.com>
2017-03-17 16:32:33 -0400

    Added topic logger node. Logs each topic's messages to it's own file under .ros/logs/latest

---
+++ src/nodes/master/topic_logger/include/topic_logger/ros_interface.h
@@ -0,0 +1,64 @@
+// State machine ROS Details
+#ifndef ROS_INTERFACE_H 
+#define ROS_INTERFACE_H
+
+#include "ros/ros.h"
+#include "relay_board/RelayDataMsg.h"
+#include "compass/CompassDataMsg.h"
+#include "camera_node/CameraDataMsg.h"
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+
+void startROS(int argc, char **argv){
+    //initilize ROS and the Node
+    ros::init(argc, argv, "topic_logger");
+}
+
+void endROS(){
+    //Shut down ROS
+    ros::shutdown();
+}
+
+
+//this class should be abstracted so all of our nodes can use it instead of copying it for each node
+class ROSInterface {
+
+    ros::NodeHandle InterfaceHandle;
+public:
+    ros::Subscriber relay_sub;
+    ros::Subscriber compass_sub;
+    ros::Subscriber camera_sub;
+
+
+  public:
+
+    ROSInterface(
+      void (*relay_cb)(const relay_board::RelayDataMsg&),
+      void (*compass_cb)(const compass::CompassDataMsg&),
+      void (*camera_cb)(const camera_node::CameraDataMsg&)
+    ) {
+
+        
+
+        // Subscribers, with callbacks
+	    relay_sub   = InterfaceHandle.subscribe("RelayData", 1, relay_cb);
+	    compass_sub = InterfaceHandle.subscribe("CompassData", 1, compass_cb);
+	    camera_sub  = InterfaceHandle.subscribe("CameraData", 1, camera_cb);
+
+    }
+
+    //lets us know if we should stop the loop
+    bool isNodeRunning(){
+        return ros::ok();
+    }
+
+
+};
+
+
+
+
+#endif
---
+++ src/nodes/master/topic_logger/src/main.cpp
@@ -0,0 +1,91 @@
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+
+#include "topic_logger/ros_interface.h"
+
+//globals (yuck) for the various file objects
+//TODO study the callback functions and figure out how to pass the data without globals
+std::ofstream relay_file;
+std::ofstream compass_file;
+std::ofstream camera_file;
+
+
+
+//Write to log file
+void writeData(std::ofstream& file, std::string data) {
+    //get current time
+    std::time_t t = time(0);
+    struct tm * now = localtime( & t );
+
+    //concatinate a nice date string
+    std::string date_str = "["
+                         //TODO add time here
+                         + std::to_string(now->tm_year + 1900) + '-'
+                         + std::to_string(now->tm_mon + 1) + '-'
+                         + std::to_string(now->tm_mday)
+                         + "]";
+
+    file << date_str << " " << data << std::endl;
+}
+
+
+
+//###Define ROS message callbacks so we can log messages as they come in.
+
+void relay_callback(const relay_board::RelayDataMsg& relayStatusMessage) {
+    std::string data = "";
+
+    writeData(relay_file, data);
+}
+
+
+void compass_callback(const compass::CompassDataMsg& compassMessage) {
+    std::string data = "Compass heading: " + std::to_string(compassMessage.heading);
+
+    writeData(compass_file, data);
+}
+
+void camera_callback(const camera_node::CameraDataMsg& cameraMessage) {
+    std::string data = "Camera direction: " + std::to_string(cameraMessage.direction) + '\n'
+                     + "	distance: " + std::to_string(cameraMessage.distance) + '\n'
+                     + "	valid: " + std::to_string(cameraMessage.tracking);
+    
+}
+
+
+
+
+int main(int argc, char **argv) {
+
+    //function defined in ros_interface header
+    startROS(argc, argv);
+
+    //setup ros interface, state machine, and vehicle data  objects
+    ROSInterface ros_interface( &relay_callback, &compass_callback, &camera_callback);
+
+    //open log files
+    
+    //cwd is /home/<user>/.ros
+    //base path goes from there to the log folder
+    //TODO maybe put it one more directory down like "sensor_data"
+    std::string base_path = "log/latest/";
+
+    relay_file = std::ofstream(base_path+"relay_board_msg.log", std::ios_base::app);
+    compass_file = std::ofstream(base_path+"compass_msg.log", std::ios_base::app);
+    camera_file = std::ofstream(base_path+"camera_msg.log", std::ios_base::app);
+
+    //start main loop (just to keep the program running while ROS is running)
+    while (ros_interface.isNodeRunning()) {
+        ros::spinOnce();  // Need to spin if we use callbacks, and to let ROS know we are alive
+        //std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+
+    //close the opened files
+    relay_file.close();
+    compass_file.close();
+    camera_file.close();
+    
+}
