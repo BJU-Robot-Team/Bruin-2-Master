1a9c2755032f3494a028709257933edc6d1e8165
Nathan Collins <nathantheinventor@gmail.com>
2018-05-22 09:50:24 -0400

    This requires sudo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # install some stuff from apt
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y g++ python python3 cmake build-essential git mercurial screen putty python3-pip python3-pyqt5 pyqt5-dev-tools gcc make
-pip3 install screeninfo
+sudo pip3 install screeninfo
 
 # install ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
